f284f30068615ee734a9f158d1770c3c71700c3f
Abhinav Thakur <thakurabhinav16160@gmail.com>
2025-04-05 11:43:12 +0530

    func

---
+++ 01basics/myFunction.ts
@@ -0,0 +1,23 @@
+function addTwo (num:number){
+    return num + 2;
+}
+
+function getUpper(val:string){
+    return val.toUpperCase();
+
+}
+
+function signUpUser(name:string,email:string,isPaid:boolean){
+
+}
+
+let loginUser = (name:string,email:string,isPaid:boolean=false)=>{
+
+}
+
+addTwo(2);
+getUpper("hitesh");
+signUpUser("abhinav","thakurhitesh@gmail.com",true)
+loginUser("abhinav","thakurhitesh@gmail.com")  
+
+export {};
